3c255d1ae8641adc4b500784a112d561c186337d
iki55 <aleksovska.isidora@gmail.com>
2024-01-12 23:03:36 +0100

    first commit

---
+++ src/proekt_30/Honda.java
@@ -0,0 +1,7 @@
+package proekt_30;
+
+class Honda extends Bike {
+void run() {System.out.println("running safely...");}
+	}
+
+
---
+++ src/proekt_30/TestAbstraction2.java
@@ -0,0 +1,12 @@
+package proekt_30;
+
+public class TestAbstraction2 {
+	public static void main(String[] args) {
+		Bike obj=new Honda();
+		obj.run();
+		obj.changeGear();
+		}
+
+}
+	
+
